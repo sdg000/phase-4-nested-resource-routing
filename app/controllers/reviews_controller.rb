869b90987f93ecc670a291b7ce3eb84d91e933c2
Seth David Gyimah <esec26@gmail.com>
2022-10-15 08:46:06 +0000

    completed code along requirements

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,16 @@
 class ReviewsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  # INDEX ACTION TO HANDLE BOTH MAIN REVIEWS RESOURCE ROUTE and NESTED REVIEWS ROUTES
+  # if a dog_house_id params is present, find and return all it's associated reviews
+  # or if dog_house_id params is not present, return all reviews.
   def index
-    reviews = Review.all
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all 
+    end
     render json: reviews, include: :dog_house
   end
 
